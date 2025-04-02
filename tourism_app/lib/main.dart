7e0f5c847dd3f98c67c23c38a2739be4223e706b
anggii <anggabos120@gmail.com>
2025-04-02 16:30:55 +0700

    API - Provider

--- tourism_app/lib/main.dart
+++ tourism_app/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tourism_app/provider/detail/bookmark_list_provider.dart';
+import 'package:tourism_app/provider/detail/tourism_detail_provider.dart';
+import 'package:tourism_app/provider/home/tourism_list_provider.dart';
 import 'package:tourism_app/provider/index_nav_provider.dart';
 import 'package:tourism_app/screen/detail/detail_screen.dart';
 import 'package:tourism_app/screen/main/main_screen.dart';
 import 'package:tourism_app/static/navigation_route.dart';
 import 'package:tourism_app/style/theme/tourism_theme.dart';
+import 'package:tourism_app/data/api/api_service.dart';
 
 void main() {
   runApp(
@@ -13,6 +16,14 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (context) => IndexNavProvider()),
         ChangeNotifierProvider(create: (context) => BookmarkListProvider()),
+        Provider(create: (context) => ApiService()),
+        ChangeNotifierProvider(
+          create: (context) => TourismListProvider(context.read<ApiService>()),
+        ),
+        ChangeNotifierProvider(
+          create:
+              (context) => TourismDetailProvider(context.read<ApiService>()),
+        ),
       ],
       child: const MyApp(),
     ),
